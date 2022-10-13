d2251ea5aa14c35c63e62c76079ef558c9e2912e
ademtarhan <ademtarhan.dev@gmail.com>
2022-10-13 13:58:01 +0300

    create-project

---
+++ WeatherApp/WeatherApp/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Adem Tarhan on 13.10.2022.
+//
+
+import UIKit
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+        var rootViewController: UIViewController? {
+            get { window?.rootViewController }
+            set {
+                window?.rootViewController = newValue
+                window?.makeKeyAndVisible()
+            }
+        }
+
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            FirebaseApp.configure()
+            initWindow()
+            initRoot()
+
+            return true
+        }
+
+        /// - Initializing window
+        private func initWindow() {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+        }
+
+        /// - Initializing root
+        private func initRoot() {
+            /*let home = HomeRouter.shared.home
+            let login = LogInRouter.shared.logIn
+            let root = UINavigationController(rootViewController: login as! UIViewController)
+            rootViewController = root
+             */
+        }
+
+}
+
